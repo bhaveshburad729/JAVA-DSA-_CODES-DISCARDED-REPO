fb8bcd7f8a9c8d4b0815a68e59eab2d1ca905760
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-07-23 23:23:48 +0530

    day 12

---
+++ bubble_sort.java
@@ -0,0 +1,37 @@
+public class bubble_sort {
+    public static void BubbleSort(int array[]) {
+        int n = array.length;
+        for (int turn = 0; turn < n - 1; turn++) {
+            int swap = 0;
+            for (int j = 0; j < n - 1 - turn; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swap++;
+                }
+            }
+            if (swap == 0) {
+                System.out.println("array is already sorted");
+                break;
+            }
+            System.out.println("swap of  " + turn + "  is - " + swap);
+        }
+
+    }
+
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int array[] = { 1, 2, 11, 4, 5, 6, 7, 8, 9, 10 };
+        BubbleSort(array);
+        printArray(array);
+    }
+
+}
