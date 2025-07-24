a33bd41748e944bcd99a58507698953b9573cbec
bhaveshburad729 <bhaveshburad729@gmail.com>
2025-07-24 11:33:36 +0530

    DAY 13

---
+++ Insertion_sort.java
@@ -0,0 +1,26 @@
+public class Insertion_sort {
+    public static void InsertionSort(int array[]) {
+        int n = array.length;
+        for (int i = 1; i <= n - 1; i++) {
+            int curr = array[i];
+            int prev = i - 1;
+            while (prev >= 0 && array[prev] > curr) {
+                array[prev + 1] = array[prev];
+                prev--;
+            }
+            array[prev + 1] = curr;
+        }
+    }
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int array[] = { 5, 4, 1, 3, 2 };
+        InsertionSort(array);
+         printArray(array);
+    }
+}
